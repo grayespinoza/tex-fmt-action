78f7133b3de4297d5a49458413084c02a589d530
Grey <grey@grayespinoza.dev>
2025-03-26 22:36:54 -0700

    use opts variable in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
 
-find . -name "*.tex" -o -name "*.bib" -o -name "*.cls" -o -name "*.sty" | xargs tex-fmt $*
+find . -name "*.tex" -o -name "*.bib" -o -name "*.cls" -o -name "*.sty" | xargs tex-fmt $INPUT_OPTS
 
 exit 0
