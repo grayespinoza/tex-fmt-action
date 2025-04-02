c035b91f0950040fe559a780d4ffc8e423aa6ed1
Grey <grey@grayespinoza.dev>
2025-04-01 17:55:10 -0700

    only run tex-fmt if tex files exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
 
-find . -name "*.tex" -o -name "*.bib" -o -name "*.cls" -o -name "*.sty" | xargs tex-fmt $INPUT_OPTS
+find . -name "*.tex" -o -name "*.bib" -o -name "*.cls" -o -name "*.sty" | xargs -r tex-fmt $INPUT_OPTS
 
 exit 0
