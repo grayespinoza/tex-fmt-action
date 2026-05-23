a25938b29fe7ed39296d3243a3c3e0c632427ae0
Grey <grey@grayespinoza.dev>
2026-05-23 14:20:22 -0700

    fix: correct bash shebang

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
-#!/bin/bash -euo pipefail
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
 
 find "$INPUT_INPUTS" -name "*.tex" -o -name "*.bib" -o -name "*.cls" -o -name "*.sty" | xargs -r tex-fmt $INPUT_OPTS
